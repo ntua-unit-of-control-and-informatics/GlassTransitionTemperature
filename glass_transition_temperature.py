9227efd64c8b8e0d9db6bf6aff20b6fa4443d507
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-04-16 10:12:33 +0200

    Update glass_transition_temperature.py

--- glass_transition_temperature.py
+++ glass_transition_temperature.py
@@ -1,1 +1,15 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import streamlit as st
+
+def density_hyperbola(x, rho0, t0, alpha, bita, gamma):
+    dT = x - t0
+    H0 = 0.5 * dT + np.sqrt(0.25 * dT * dT + np.exp(gamma))
+    rho = rho0 - alpha * dT - bita * H0
+    return rho
+
+st.header("Glass transition temperaature from density vs temperature data")
+
+st.text("This WebApp was created by Evangelos Voyiatzis.")
 
