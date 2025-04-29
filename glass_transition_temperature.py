b04dcff8359d420f95c06e589f9f89ff83731361
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-04-29 16:36:16 +0200

    Update glass_transition_temperature.py

--- glass_transition_temperature.py
+++ glass_transition_temperature.py
@@ -31,22 +31,22 @@ with st.form("myform"):
                 y = [float(i) for i in input_stream[1::2]]
             else:
                 y = [1.0/float(i) for i in input_stream[1::2]]
-            st.session_state['data'] = pd.DataFrame({'x': x, 'y': y})
+            st.session_state['data'] = pd.DataFrame({'Temperature': x, 'y': y})
         else:
             st.write("you need to upload a valid txt or csv file")
 
 # Plot the data
 if 'data' in st.session_state:
     with st.form("myform2"):
-        fig1 = alt.Chart(st.session_state['data']).mark_point(filled=True).encode(x='x',y='y') 
+        fig1 = alt.Chart(st.session_state['data']).mark_point(filled=True).encode(x='Temperature',y='y') 
         #st.scatter_chart(st.session_state['data'].set_index('x'), x_label = "Temperature", y_label = "Density")
         submit2 = st.form_submit_button("Fit hyberpola")
         if submit2:
-            param, param_cov = curve_fit(density_hyperbola, st.session_state['data']['x'], st.session_state['data']['y'])
-            y_from_fitting = density_hyperbola(st.session_state['data']['x'], param[0], param[1], param[2], param[3], param[4])
-            st.session_state['data_from_fitting'] = pd.DataFrame({'x': st.session_state['data']['x'], 'y_from_fitting': y_from_fitting})
+            param, param_cov = curve_fit(density_hyperbola, st.session_state['data']['Temperature'], st.session_state['data']['y'])
+            y_from_fitting = density_hyperbola(st.session_state['data']['Temperature'], param[0], param[1], param[2], param[3], param[4])
+            st.session_state['data_from_fitting'] = pd.DataFrame({'temperature': st.session_state['data']['Temperature'], 'y_from_fitting': y_from_fitting})
             #st.scatter_chart(st.session_state['data_from_fitting'].set_index('x'))
-            fig2 = alt.Chart(st.session_state['data_from_fitting']).mark_line(color='red').encode(x='x',y='y_from_fitting')
+            fig2 = alt.Chart(st.session_state['data_from_fitting']).mark_line(color='red').encode(x='temperature',y='y_from_fitting')
             st.altair_chart((fig1 + fig2).interactive())
             st.write(f"The predicted glass transition temperature T\u2080 is {param[1]:.4f} ")
             st.write(f"The fitted values of the parameters are: \n\
