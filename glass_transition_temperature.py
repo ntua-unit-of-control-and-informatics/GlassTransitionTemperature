6a4f33212aa8aca85e1a50e21a6900f7b1757371
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-04-29 16:24:20 +0200

    Update glass_transition_temperature.py

--- glass_transition_temperature.py
+++ glass_transition_temperature.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import altair as alt
 import numpy as np
 from scipy.optimize import curve_fit
 import pandas as pd
@@ -37,19 +38,24 @@ with st.form("myform"):
 # Plot the data
 if 'data' in st.session_state:
     with st.form("myform2"):
-        st.scatter_chart(st.session_state['data'].set_index('x'), x_label = "Temperature", y_label = "Density")
+        fig1 = alt.Chart(st.session_state['data']).mark_point(filled=True).encode(x='temperature',y='density') 
+        #st.scatter_chart(st.session_state['data'].set_index('x'), x_label = "Temperature", y_label = "Density")
         submit2 = st.form_submit_button("Fit hyberpola")
         if submit2:
             param, param_cov = curve_fit(density_hyperbola, st.session_state['data']['x'], st.session_state['data']['y'])
             y_from_fitting = density_hyperbola(st.session_state['data']['x'], param[0], param[1], param[2], param[3], param[4])
             st.session_state['data_from_fitting'] = pd.DataFrame({'x': st.session_state['data']['x'], 'y_from_fitting': y_from_fitting})
-            st.scatter_chart(st.session_state['data_from_fitting'].set_index('x'))
+            #st.scatter_chart(st.session_state['data_from_fitting'].set_index('x'))
+            fig2 = alt.Chart(st.session_state['data_from_fitting']).mark_line(color='red').encode(x='temperature',y='density')
+            st.altair_chart((fig1 + fig2).interactive())
             st.write(f"The predicted glass transition temperature T\u2080 is {param[1]:.4f} ")
             st.write(f"The fitted values of the parameters are: \n\
             \N{GREEK SMALL LETTER RHO}\u2080: {param[0]:.4f}\n\
             \N{GREEK SMALL LETTER ALPHA}: {param[2]:.4f}\n\
             \N{GREEK SMALL LETTER BETA}: {param[3]:.4f}\n\
             \N{GREEK SMALL LETTER GAMMA}: {param[4]:.4f}")
+        else:
+            st.altair_chart(fig1.interactive())
 
 if st.button("Reset"):
     if 'data' in st.session_state:
